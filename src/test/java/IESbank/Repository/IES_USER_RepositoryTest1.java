1ec00bc3034f3a600a8c7e16057a36ffe9601ca6
ayush <ayushtiwari.4lz1@gmail.com>
2024-09-11 15:59:12 +0530

    files hving the issue

---
+++ src/test/java/IESbank/Repository/IES_USER_RepositoryTest1.java
@@ -0,0 +1,21 @@
+package IESbank.Repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IES_USER_RepositoryTest1 {
+
+    @Test
+    public void updateAccStatus() {
+
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByEmailAndPwd() {
+    }
+}
---
+++ src/test/java/IESbank/Service/ACCOUNT_Service_ImplTest2.java
@@ -0,0 +1,39 @@
+package IESbank.Service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ACCOUNT_Service_ImplTest2 {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public  void createUserAccount() {
+
+    }
+
+    @Test
+    void fetchUserAccounts() {
+    }
+
+    @Test
+    void getUserAccById() {
+    }
+
+    @Test
+    void changeAccStatus() {
+    }
+
+    @Test
+    void unlockUserAccount() {
+    }
+}
